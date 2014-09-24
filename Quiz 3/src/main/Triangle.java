0624ce1ab4562f9f6fce6f1bf183c8c72c7d1dab
npompetti <npompetti@wifi-roaming-128-4-176-112.host.udel.edu>
2014-09-24 18:25:32 -0400

    First

---
+++ Quiz 3/src/main/Triangle.java
@@ -0,0 +1,75 @@
+package main;
+
+import java.util.Arrays;
+import java.lang.Math.*;
+
+public class Triangle {
+	
+	double side1 = 1.0;
+	double side2= 1.0;
+	double side3= 1.0;
+	
+	//No arg constructor
+	public Triangle(){}
+	
+	//Three arg constructor
+	public Triangle(double side_1, double side_2, double side_3){
+		this.side1 = side_1;
+		this.side2 = side_2;
+		this.side3 = side_3;
+	}
+	
+	//Getters for all attributes
+	public double get_side1(){
+		return this.side1;
+	}
+	
+	public double get_side2(){
+		return this.side2;
+	}
+	
+	public double get_side3(){
+		return this.side3;
+	}
+	
+	public static double getArea(Triangle tri){
+		//Creating array to sort the sides
+		double[] side_list = new double[3];
+		double height;
+		double base;
+		
+		side_list[0]= tri.get_side1();
+		side_list[1]= tri.get_side2();
+		side_list[2]= tri.get_side3();
+		
+		Arrays.sort(side_list);
+		
+		base = side_list[2];
+		
+		double temp= base/2;
+		
+		if(temp > side_list[1]){
+			height = Math.sqrt(Math.pow(temp, 2)-Math.pow(side_list[1],2));
+		}
+		
+		else{
+			height = Math.sqrt(Math.pow(side_list[1], 2)-Math.pow(temp,2));
+		}
+		
+		double area = temp * height;
+		
+		return area;
+		
+	}
+	
+	public static void main(String[] args) {
+		Triangle tri = new Triangle(3,4,5);
+		double[] side_list = new double[3];
+		side_list[1]= tri.get_side1();
+		
+		//System.out.print(side_list[1]/2);
+		System.out.println(getArea(tri));
+		}
+	}
+
+
